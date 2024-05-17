69b77857c898372fa00b812c8865b084de3f08b2
Souheab <souheab@protonmail.com>
2024-05-17 14:04:44 -0400

    Analyze html using bs4 instead of selenium

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from typing import List
 from decimal import Decimal
 import pandas as pd
+import time
+import random
 import os
+from bs4 import BeautifulSoup
 
 
 class Album:
@@ -71,6 +76,7 @@ class Album:
     def __str__(self):
         return self.__repr__()
 
+
 def save_albums_data_to_csv(*albums, file_path="albums.csv"):
     df = pd.DataFrame([album.to_dict() for album in albums])
     if os.path.exists(file_path):
@@ -78,105 +84,115 @@ def save_albums_data_to_csv(*albums, file_path="albums.csv"):
     else:
         df.to_csv(file_path, index=False)
 
-def get_rym_top_albums_data(page=1, driver=webdriver.Chrome()):
-    #    driver.get(f"https://rateyourmusic.com/charts/top/album/all-time/{page}/")
-    #
-    file_path = os.path.abspath("./mock.html")
-    driver.get(f"file://{file_path}")
-    driver.implicitly_wait(1)
-    elements = driver.find_elements(By.CLASS_NAME, "page_section_charts_item_wrapper")
+
+def get_rym_top_albums_mock_html_source():
+    with open("mock.html", "r", encoding="utf-8") as file:
+        html_source = file.read()
+
+    return html_source
+
+
+def get_rym_top_albums_html_source(driver, page=1):
+    driver.get(f"https://rateyourmusic.com/charts/top/album/all-time/{page}/")
+
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.CLASS_NAME, "page_section_charts_item_wrapper")
+            )
+        )
+    except TimeoutException:
+        print("Timeout while waiting for page to load")
+        return []
+
+    html_source = driver.page_source
+    driver.get("about:blank")
+    return html_source
+
+
+def get_rym_top_albums_elements(html_source: str):
+    soup = BeautifulSoup(html_source, "html.parser")
+    elements = soup.find_all("div", class_="page_section_charts_item_wrapper")
     return elements
 
 
-def get_album_rank(element: WebElement):
-    element_id = element.get_attribute("id")
+def get_album_rank(element) -> int:
+    element_id = element.get("id")
     return int(element_id[3:])
 
 
-def get_album_name(element: WebElement):
-    album_name_parent_element: WebElement = element.find_element(
-        By.CLASS_NAME, "page_charts_section_charts_item_title"
+def get_album_name(element) -> str:
+    album_name_parent_element = element.find(
+        "div", class_="page_charts_section_charts_item_title"
     )
-    return album_name_parent_element.text
+    return album_name_parent_element.text.strip()
 
 
-def get_album_artists(element: WebElement):
-    album_artists_parent_element = element.find_elements(By.CLASS_NAME, "artist")
+def get_album_artists(element) -> List[str]:
+    album_artists_parent_elements = element.find_all("a", class_="artist")
     album_artists_names = [
-        element.text[:-1].strip() if element.text.endswith("&") else element.text
-        for element in album_artists_parent_element
+        el.text[:-1].strip() if el.text.endswith("&") else el.text.strip()
+        for el in album_artists_parent_elements
     ]
     return album_artists_names
 
 
-def get_album_num_ratings(element: WebElement):
-    num_ratings = element.find_element(
-        By.CSS_SELECTOR, ".page_charts_section_charts_item_details_ratings .full"
+def get_album_num_ratings(element) -> int:
+    num_ratings_element = element.select_one(
+        ".page_charts_section_charts_item_details_ratings .full"
     )
-    num_ratings = int(num_ratings.text.strip().replace(",", ""))
-    return num_ratings
+    return int(num_ratings_element.text.strip().replace(",", ""))
 
 
-def get_album_num_reviews(element: WebElement):
-    num_reviews = element.find_element(
-        By.CSS_SELECTOR, ".page_charts_section_charts_item_details_reviews .full"
-    )
-    num_reviews = int(num_reviews.text.strip().replace(",", ""))
-    return num_reviews
-
-
-def get_album_release_date(element: WebElement):
-    release_date = element.find_element(
-        By.CSS_SELECTOR, ".page_charts_section_charts_item_date > span:not([class])"
-    )
-    return release_date.text
-
-
-def get_album_primary_genres(element: WebElement):
-    genre_list = [
-        element.text
-        for element in element.find_elements(
-            By.CSS_SELECTOR, ".page_charts_section_charts_item_genres_primary > .genre"
+def get_album_num_reviews(element) -> int:
+    try:
+        num_reviews_element = element.select_one(
+            ".page_charts_section_charts_item_details_reviews .full"
         )
-    ]
-    return genre_list
+        return int(num_reviews_element.text.strip().replace(",", ""))
+    except:
+        # There might be no reviews which will raise an exception, so return 0
+        return 0
 
 
-def get_album_secondary_genres(element: WebElement):
-    genre_list = [
-        element.text
-        for element in element.find_elements(
-            By.CSS_SELECTOR,
-            ".page_charts_section_charts_item_genres_secondary > .genre",
-        )
-    ]
-    return genre_list
-
-
-def get_album_average_score(element: WebElement):
-    average_score = element.find_element(
-        By.CLASS_NAME, "page_charts_section_charts_item_details_average_num"
+def get_album_release_date(element) -> str:
+    release_date_element = element.select_one(
+        ".page_charts_section_charts_item_date > span:not([class])"
     )
-    average_score = Decimal(average_score.text.strip())
-    return average_score
+    return release_date_element.text.strip()
 
 
-def get_album_top_descriptors(element: WebElement):
-    descriptors = [
-        element.text
-        for element in element.find_elements(
-            By.CSS_SELECTOR,
-            ".page_charts_section_charts_item_genre_descriptors > .comma_separated",
-        )
-    ]
-    return descriptors
+def get_album_primary_genres(element) -> List[str]:
+    genre_elements = element.select(
+        ".page_charts_section_charts_item_genres_primary > .genre"
+    )
+    return [el.text.strip() for el in genre_elements]
 
 
-def get_album_rym_href(element: WebElement):
-    rym_url = element.find_element(
-        By.CLASS_NAME, "page_charts_section_charts_item_link"
-    ).get_attribute("href")
-    return rym_url
+def get_album_secondary_genres(element) -> List[str]:
+    genre_elements = element.select(
+        ".page_charts_section_charts_item_genres_secondary > .genre"
+    )
+    return [el.text.strip() for el in genre_elements]
+
+
+def get_album_average_score(element) -> Decimal:
+    average_score_element = element.find(
+        "span", class_="page_charts_section_charts_item_details_average_num"
+    )
+    return Decimal(average_score_element.text.strip())
+
+
+def get_album_top_descriptors(element) -> List[str]:
+    descriptor_elements = element.select(
+        ".page_charts_section_charts_item_genre_descriptors > .comma_separated"
+    )
+    return [el.text.strip() for el in descriptor_elements]
+
+
+def get_album_rym_href(element) -> str:
+    rym_url_element = element.find("a", class_="page_charts_section_charts_item_link")
+    return rym_url_element.get("href")
 
 
 def generate_album_data(elements: List[WebElement]):
@@ -193,27 +209,37 @@ def generate_album_data(elements: List[WebElement]):
         secondary_genres = get_album_secondary_genres(element)
         descriptors = get_album_top_descriptors(element)
         rym_href = get_album_rym_href(element)
-        list.append(
-            Album(
-                rank,
-                title,
-                artists,
-                average_score,
-                num_ratings,
-                num_reviews,
-                release_date,
-                primary_genres,
-                secondary_genres,
-                descriptors,
-                rym_href,
-            )
+        album = Album(
+            rank,
+            title,
+            artists,
+            average_score,
+            num_ratings,
+            num_reviews,
+            release_date,
+            primary_genres,
+            secondary_genres,
+            descriptors,
+            rym_href,
         )
-        print(f"Created \"{title}\" album object")
+        print(f'Created "{title}" album object')
+        list.append(album)
 
     return list
 
 
 if __name__ == "__main__":
-    elements = get_rym_top_albums_data()
-    list = generate_album_data(elements)
-    save_albums_data_to_csv(*list)
+    for i in range(38, 40):
+        # Have to recreate the driver every time to not get blocked
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(1)
+        print(f"Getting data from page {i}")
+        html_source = get_rym_top_albums_html_source(driver, i)
+        driver.quit()
+        elements = get_rym_top_albums_elements(html_source)
+        list = generate_album_data(elements)
+        save_albums_data_to_csv(*list)
+        print(f"Page {i} written to CSV file.")
+        sleep_time = random.uniform(1, 3)
+        print(f"Sleeping for {sleep_time} seconds")
+        time.sleep(sleep_time)
